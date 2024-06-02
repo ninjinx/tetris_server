9a7aa9cebfeb6203abb34ae0d4035d991bf15de9
ninjinx <responsura@gmail.com>
2024-06-02 10:35:41 +0900

    interfaceの作成

---
+++ Shared/Interfaces/IGamingHub.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using MagicOnion;
+using MessagePack;
+using UnityEngine;
+
+namespace Shared.Interfaces
+{
+    public interface IGamingHubReceiver
+    {
+        void OnJoin(Player player);
+        void OnLeave(Player player);
+        void OnMove(Player player);
+    }
+
+    public interface IGamingHub : IStreamingHub<IGamingHub, IGamingHubReceiver>
+    {
+        ValueTask<Player[]> JoinAsync(string roomName, string userName, Vector3 position, Quaternion rotation);
+        ValueTask LeaveAsync();
+        ValueTask MoveAsync(Vector3 position, Quaternion rotation);
+    }
+
+    [MessagePackObject]
+    public class Player
+    {
+        [Key(0)]
+        public string Name { get; set; }
+        [Key(1)]
+        public Vector3 Position { get; set; }
+        [Key(2)]
+        public Quaternion Rotation { get; set; }
+    }
+}
---
+++ Shared/Interfaces/IMyFirstService.cs
@@ -0,0 +1,8 @@
+﻿using MagicOnion;
+namespace Shared.Interfaces
+{
+    public interface IMyFirstService : IService<IMyFirstService>
+    {
+        UnaryResult<int> SumAsync(int x, int y);
+    }
+}
